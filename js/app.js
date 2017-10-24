c5fdd3490c10de6f03bdb5abed884dffbdc98cf1
Davy <david.j.butterworth@gmail.com>
2017-10-24 18:48:34 -0400

    Added Javascript Slideshow

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
 /*JavaScript*/
+
+/*Adapted Javascript for Slideshow*/
+
+/*Changes slide upon clicking image*/
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+/*Changes slide upon clicking dot*/
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+/*Slideshow Function*/
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
